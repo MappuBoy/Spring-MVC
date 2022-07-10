3094eec6046952460d9b8a83a8993f57f32598b6
MappuBoy <pituvaraliyange@gmail.com>
2022-07-10 10:34:58 +0530

    first init

---
+++ src/main/java/lk/ijse/spring/config/JPAConfig.java
@@ -0,0 +1,47 @@
+package lk.ijse.spring.config;
+@Configuration
+@EnableJpaRepositories(basePackages = "lk.ijse.spring.repo")
+@EnableTransactionManagement
+@PropertySource("classpath:application.properties")
+
+
+public class JPAConfig {
+    @Autowired
+    Environment env;
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter va) {
+        LocalContainerEntityManagerFactoryBean bean = new LocalContainerEntityManagerFactoryBean();
+        bean.setJpaVendorAdapter(va);
+        bean.setDataSource(ds);
+        bean.setPackagesToScan(env.getRequiredProperty("entity.package.name"));
+        return bean;
+    }
+
+    @Bean
+    public DataSource dataSource() throws NamingException {
+        //Driver Manager Data Source
+        DriverManagerDataSource dataSource= new DriverManagerDataSource();
+        dataSource.setUrl(env.getRequiredProperty("my.app.url"));
+        dataSource.setUsername(env.getRequiredProperty("my.app.username"));
+        dataSource.setPassword(env.getRequiredProperty("my.app.password"));
+        dataSource.setDriverClassName(env.getRequiredProperty("my.app.driverclassname"));
+        return dataSource;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter vendor = new HibernateJpaVendorAdapter();
+        vendor.setDatabasePlatform(env.getRequiredProperty("my.app.dialect"));
+        vendor.setDatabase(Database.MYSQL);
+        vendor.setShowSql(true);
+        vendor.setGenerateDdl(true);
+        return vendor;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+
+}
---
+++ src/main/java/lk/ijse/spring/controller/CustomerController.java
@@ -0,0 +1,17 @@
+package lk.ijse.spring.controller;
+
+import lk.ijse.spring.dto.CustomerDTO;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("customer")
+public class CustomerController {
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public CustomerDTO getAllCustomer(){
+        return new CustomerDTO("C001","Navod","Thalawathugoda",25000.00);
+    }
+}
