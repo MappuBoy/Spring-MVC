ffc7008932a1f882835879f2ca294eb81f006c34
mappuboy <pituvaraliyange@gmail.com>
2022-07-13 13:42:29 +0530

    init

--- src/main/java/lk/ijse/spring/dto/CustomerDTO.java
+++ src/main/java/lk/ijse/spring/dto/CustomerDTO.java
@@ -1,53 +1,19 @@
 package lk.ijse.spring.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+//if this is entity add @Entity
 public class CustomerDTO {
+    //@id
     private String id;
     private String name;
     private String address;
     private double salary;
-
-    public CustomerDTO() {
-    }
-
-    public CustomerDTO(String id, String name, String address, double salary) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.salary = salary;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-
-
-    }
 }
